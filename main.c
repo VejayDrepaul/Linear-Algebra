999cd187ceba5971c7dbab9dfeb53c6e812632d3
Vejay Drepaul <vejayddrepaul@gmail.com>
2023-01-29 19:45:31 -0500

    Vector files working

--- main.c
+++ main.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "vector.h"
+#include "martix.h"
 
 int main(void) {
     double u[] = {1, 2, 3, 4};
     double v[] = {1, 2, 3, 4};
 
-    arithmetic(u, '+', v);
+    double *result = arithmetic(u, '+', v);
+    result[2] = 3;
+    for (int i = 0; i < 4; i++) {
+        printf("%lf\n", result[i]);
+    }
 
     return 0;
 }
